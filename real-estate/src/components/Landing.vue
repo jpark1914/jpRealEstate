22068713cad6705bcf4d025337ce46a75590979e
Jordan Parker <parkerj4321@gmail.com>
2018-12-19 14:37:15 -0600

    Added images

--- real-estate/src/components/Landing.vue
+++ real-estate/src/components/Landing.vue
@@ -13,35 +13,22 @@
                     @sliding-end="onSlideEnd">
 
             <!-- Text slides with image -->
-            <b-carousel-slide caption="First slide"
-                              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-                              img-src="https://picsum.photos/1024/480/?image=52"
-            ></b-carousel-slide>
+            <b-carousel-slide  class="homeimage" img-src="alexandra-gorn-485551-unsplash.jpg">
+            </b-carousel-slide>
 
             <!-- Slides with custom text -->
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-                <h1>Hello world!</h1>
+            <b-carousel-slide class="homeimage" img-src="rawpixel-1053187-unsplash.jpg">
             </b-carousel-slide>
 
             <!-- Slides with image only -->
-            <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58">
+            <b-carousel-slide class="homeimage" img-src="alberto-castillo-q-346020-unsplash.jpg">
             </b-carousel-slide>
 
             <!-- Slides with img slot -->
             <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-            <b-carousel-slide>
-                <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
-                     src="https://picsum.photos/1024/480/?image=55" alt="image slot">
+            <b-carousel-slide class="homeimage" img-src="scott-webb-167099-unsplash.jpg">
             </b-carousel-slide>
 
-            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-            <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                    eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
-                    ut lacus vel interdum.
-                </p>
-            </b-carousel-slide>
 
         </b-carousel>
 
@@ -70,9 +57,17 @@
                 this.sliding = false
             }
         },
+        image:{
+            image1: '../assests/alexandra-gorn-485551-unsplash.jpg'
+        }
     }
 </script>
 
 <style scoped>
+
+    .homeimage{
+        height: 30rem;
+        background: top fixed;
+    }
 
 </style>
